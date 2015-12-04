2819c1c58ec04efa55b5843648b25aaf036ebc52
Faline10 <fawn.be@gmail.com>
2015-12-03 20:08:21 -0800

    Add boilerplate socket.io

--- server/sockets.js
+++ server/sockets.js
@@ -6,8 +6,33 @@ var connect = function(eventUrl, io) {
 
   mealEvent.once('connection', function(socket) {
 
+
   });
 };
 
 // Required by [server.js]
 module.exports = connect;
+
+      // won't need this unless using user auth:
+      // io.use(sharedsession(session));
+
+      // io.on('connection', function (socket) {
+      //   console.log("connected to socket");
+
+      //   // ** ---- server-side
+
+      //   socket.emit('newappcreated', { hello: 'world' }); //what is hello: world?
+        
+      //   socket.on('userAdded', function(user){
+      //     io.emit('userAdded', user);
+      //   });
+
+      //   socket.on('my other event', function (data) {
+      //     console.log(data);
+      //   });
+
+      //   // only if using handshake sessions for user auth
+      //   // socket.on("login", function(data){
+      //   //       console.log("client["+socket.handshake.session.myCustomData.userID+"] sent data: " + data);
+      //   //   })
+      // });
