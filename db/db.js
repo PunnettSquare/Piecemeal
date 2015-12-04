571a33b9a98ad44cc1b404af0cacba60e247f55d
Faline10 <fawn.be@gmail.com>
2015-12-04 11:52:16 -0800

    Add boilerplate angular-socket-io, fix ui-router infinite loop, update db username and pw

--- db/db.js
+++ db/db.js
@@ -1,10 +1,9 @@
-var dbInfo = require('../dbInfo');
 var knex = require("knex")({
   client: "pg",
   connection: {
     host: "localhost",
-    user: dbInfo.username,
-    password: dbInfo.password,
+    user: "admin",
+    password: "admin",
     database: "piecemeal"
   }
 });
