ac898187ac90130760bb28549c42b7f56d4c9a88
Michelle Lee <michelle.mh.lee92@gmail.com>
2015-12-04 12:39:56 -0800

    (feat) Add username post request, client-side small changes

--- client/app/home/home.factory.js
+++ client/app/home/home.factory.js
@@ -4,17 +4,29 @@
   angular.module('Piecemeal')
     .factory('homeFactory', homeFactory);
 
-  homeFactory.$inject = [];
+  homeFactory.$inject = ['$http'];
 
-  function homeFactory() {
+  function homeFactory($http) {
     var services = {
-      enterRoom: enterRoom
+      enterRoom: enterRoom,
+      sendSessionUser: sendSessionUser
     };
 
     return services;
 
     function enterRoom() {
 
+    }
+
+    function sendSessionUser(username) {
+      return $http({
+          method: 'POST',
+          url: '/newUser',
+          data: username
+        })
+        .then(function(res) {
+          return res.data;
+        });
     }
   }
 
