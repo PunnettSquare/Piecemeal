069801a8bd5f77a4df2ceb36286c049c093ecabb
Faline10 <fawn.be@gmail.com>
2015-12-05 15:07:13 -0800

    Add data example, update allDishes sockets and allDishes html

--- client/app/allDishes/allDishes.controller.js
+++ client/app/allDishes/allDishes.controller.js
@@ -10,30 +10,93 @@
 
     var self = this;
 
-    // join test
-    socket.on('join', function(data) {
-      console.log("receiving data for join");
+    /* format of data:
+      {
+        eventId: 1,
+        users: [
+          {
+            dishes: [{
+              cost: ,
+              dishId: ,
+              name:
+            }],
+            userId: ,
+            username:
+          },
+          {
+            dishes: [{
+              cost: ,
+              dishId: ,
+              name:
+            }],
+            userId: ,
+            username:
+          }
+        ]
+      }
+    */
+
+    /* Future format of data:
+        { eventId: 1,
+          code: 'testRoom', // <---- will be added later on
+          users: 
+           [ { username: 'Jackson',
+               status: true,
+               host: true,
+               id: 1,
+               dishes: '[{
+                 "id":1,
+                 "username":"Jackson",
+                 "dish_id":1,
+                 "user_id":1,
+                 "name":"chicken",
+                 "cost":10,"event_id":1
+               },
+               {
+                  "id":2,
+                  "username":"Jackson",
+                  "dish_id":2,
+                  "user_id":1,
+                  "name":"rice",
+                  "cost":13,
+                  "event_id":1
+                }]'
+              },
+             { username: 'Jack',
+               status: false,
+               host: false,
+               id: 2,
+               dishes: '[{
+                "id":5,
+                "username":"Jack",
+                "dish_id":5,
+                "user_id":2,
+                "name":"chicken",
+                "cost":10,
+                "event_id":1
+              },
+              {
+                "id":12,
+                "username":"Jack",
+                "dish_id":12,
+                "user_id":2,
+                "name":"protein",
+                "cost":1,
+                "event_id":1
+              }]'
+            }]
+          }]
+        }
+    */
+
+    socketFactory.on('join', function(data) {
+      console.log("----->Upon join, received this data: ", data);
+      self.listOfDishes = data;
     });
 
-    // self.listOfMeals;
-
-    // AppSocket.socket.on('join', function(data) {
-    //   self.listOfMeals = data;
-    //   console.log(data);
-    // });
-
-    // AppSocket.socket.on('dishAdded', function(data) {
-    //   self.listOfMeals.push(data);
-    // });
-
-// join
-// socketFactory.on('join', function(data) {
-//   self.listOfMeals = data;
-// });
-
-// socketFactory.on('dishAdded', function(data) {
-//   self.listOfMeals.push(data);
-// });
+    socketFactory.on('dishAdded', function(data) {
+      self.listOfMeals.push(data);
+    });
 
     socketFactory.init();
 
