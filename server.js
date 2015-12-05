bb3d28d765ef28aa18fc325b5c40f447b9b10221
Jackson <jsharf212@gmail.com>
2015-12-04 23:11:22 -0800

    finished dummy data generator and DB utils

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var handleSocket = require('./server/sockets');
 var util = require('./server/utility.js');
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
-var _ = require('underscore');
+var generateData = require('./generateData')
 app.use(bodyParser.urlencoded({
   extended: true
 }));
@@ -89,37 +89,7 @@ app.get('/*', function(req, res) {
 http.listen(port, function() {
   console.log('server listening on', port, 'at', new Date());
 });
-var roomName = 'testRoom';
-Promise.all([
-util.createEvent(db, 'testRoom')
-util.createUser(db, 'Jack', 7, false),
-util.createUser(db,'Jill', 7, false),
-util.createUser(db,'Sarah', 7, false),
-util.createUser(db,'Ted', 7, false),
-// util.findEventUsers(db, 7),
-util.createDish(db, 'chicken', 10, 12, 7),
-util.createDish(db, 'rice', 13, 13, 7),
-util.createDish(db, 'soup', 5, 14, 7),
-util.createDish(db, 'protein', 1, 15, 7),
-util.createDish(db, 'chicken', 10, 13, 7),
-util.createDish(db, 'rice', 13, 14, 7),
-util.createDish(db, 'soup', 5, 15, 7),
-util.createDish(db, 'protein', 1, 12, 7),
-util.createDish(db, 'chicken', 10, 14, 7),
-util.createDish(db, 'rice', 13, 15, 7),
-util.createDish(db, 'soup', 5, 12, 7),
-util.createDish(db, 'protein', 1, 13, 7),
-util.createDish(db, 'chicken', 10, 15, 7),
-util.createDish(db, 'rice', 13, 12, 7),
-util.createDish(db, 'soup', 5, 13, 7),
-util.createDish(db, 'protein', 1, 14, 7)
-  ])
-.then(function() {
-util.gatherState(db, 7, 'the code')
-.then(console.log)
-  
-})
-.catch(function(err){
-  console.error(err);
-})
-// util.findUserDishes(db, 7);
+
+// uncomment this to populate an empty database with dummy data from ./generateData.js
+// comment it out again after
+// setTimeout(generateData, 1000);
