0bdaf0f72b98dda9376d8a008c00eb34b8263977
Faline10 <fawn.be@gmail.com>
2015-12-07 11:15:51 -0800

    Last changes to allDishes -F

--- server/sockets.js
+++ server/sockets.js
@@ -12,15 +12,30 @@ var connect = function(eventUrl, eventInfo, io) {
 
     socket.emit('join', eventInfo);
 
+<<<<<<< HEAD
     socket.on('addDish', function(data) {
       console.log("AddDish event heard from the client!");
       
+=======
+    socket.on('addDish', function (data) {
+      console.log("AddDish event heard from the server!");
+>>>>>>> Last changes to allDishes -F
       console.log('data =', data);
       //TODO add dish to DB
       socket.broadcast.emit('dishAdded', { //or mealEvent.emit to send to all
         cost: data.cost,
         name: data.name
       });
+    });
+
+    socket.on('shareDish', function (data) {
+      console.log("User is sharing dish"); 
+      // TO DO: add user to dish's list of users in DB
+    });
+
+    socket.on('unshareDish', function (data) {
+      console.log("User is no longer sharing dish"); 
+      // TO DO: remove user from dish's list of users in DB
     });
 
   });
