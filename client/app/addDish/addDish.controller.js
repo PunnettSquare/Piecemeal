4c5828a65fad01b46fdeddd4be496846a4686a0d
Fawn Bertram <fawn.be@gmail.com>
2015-12-10 11:19:53 -0800

    remove self.test

--- client/app/addDish/addDish.controller.js
+++ client/app/addDish/addDish.controller.js
@@ -8,7 +8,6 @@
 
   function AddDishCtrl(socketFactory, addDishFactory, $location, appFactory, $scope) {
     var self = this;
-    self.test = "test"; // Remove later
 
     // When appFactory is updated, $rootScope is used as a bus to emit to user's allDishes controller $scope
     self.data = appFactory.data;
