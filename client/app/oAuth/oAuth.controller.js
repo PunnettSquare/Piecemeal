72921361e45e6a8643fec9474e0c4279e4c4c3fc
Jackson <jsharf212@gmail.com>
2015-12-12 22:30:07 -0800

    (feat) added oAuth page

---
+++ client/app/oAuth/oAuth.controller.js
@@ -0,0 +1,17 @@
+(function() {
+  'use strict';
+
+  angular.module('Piecemeal')
+  .controller('OAuthCtrl', OAuthCtrl);
+
+  OAuthCtrl.$inject = ['oAuthFactory'];
+
+  function OAuthCtrl(oAuthFactory) {
+    var self = this;
+    self.venmoAuth = function () {
+      oAuthFactory.venmoLogin();
+    }
+    
+  }
+
+})();
--- server/middleware/middleware.js
+++ server/middleware/middleware.js
@@ -9,6 +9,17 @@ module.exports = function (app, express, io) {
     extended: true
   }));
 
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin: http://localhost:8080");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    next();
+  });
+
   app.use(bodyParser.json());
 
   app.use(morgan('dev'));
