f7151c6a137cb92b7b266e9b57403378851ae039
Michelle Lee <michelle.mh.lee92@gmail.com>
2015-12-14 21:55:18 -0800

    (bugfix) Grand total cost bug fixed

--- client/app/hostBill/hostBill.controller.js
+++ client/app/hostBill/hostBill.controller.js
@@ -22,7 +22,7 @@
     self.sendBillsToGuests = function() {
       self.tipSum = (self.getSubTotal(self.data.dishes) * self.tip * 0.01) + self.getSubTotal(self.data.dishes);
       self.taxSum = (self.getSubTotal(self.data.dishes) * self.tax * 0.01) + self.getSubTotal(self.data.dishes);
-      self.grandTotal = self.getSubTotal(self.data.dishes) + self.tax + self.tipSum;
+      self.grandTotal = self.getSubTotal(self.data.dishes) + (self.tip * 0.01 * self.getSubTotal(self.data.dishes)) + (self.tax * 0.01 * self.getSubTotal(self.data.dishes));
 
       socketFactory.emit('sendBillToGuests', {
         event_id: appFactory.getSessStorage('event_id'),
