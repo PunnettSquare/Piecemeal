cf79bdb3388c92947c7eb62982f2bedd6ed7904b
Jackson <jsharf212@gmail.com>
2015-12-16 10:23:48 -0800

    (cleanup) commented out code in progress

--- client/app/app.routes.js
+++ client/app/app.routes.js
@@ -106,21 +106,21 @@
           }
         },
         resolve: {
-          getInfo: ['$http', 'appFactory', 'socketFactory', function($http, appFactory, socketFactory) {
-            if (!appFactory.data) {
-              socketFactory.init();
-              appFactory.initListeners();
-              return $http({
-                method: 'GET',
-                url: '/' + window.sessionStorage.code + '/refresh',
-              })
-              .then(function(data) {
-                console.log('data on resolve', data)
-                // appFactory.data = data.data;
-                return data.data;
-              })
-            }
-          }]
+          // getInfo: ['$http', 'appFactory', 'socketFactory', function($http, appFactory, socketFactory) {
+          //   if (!appFactory.data) {
+          //     socketFactory.init();
+          //     appFactory.initListeners();
+          //     return $http({
+          //       method: 'GET',
+          //       url: '/' + window.sessionStorage.code + '/refresh',
+          //     })
+          //     .then(function(data) {
+          //       console.log('data on resolve', data)
+          //       // appFactory.data = data.data;
+          //       return data.data;
+          //     })
+          //   }
+          // }]
         }
       })
       .state('event.allDishes', {
