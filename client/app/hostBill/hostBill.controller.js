a77c6a1702e25611c85a0b6518188040a31799a4
Michelle Lee <michelle.mh.lee92@gmail.com>
2015-12-16 22:15:54 -0800

    (bugfix) Socket refresh fixed for hostBill!!

--- client/app/hostBill/hostBill.controller.js
+++ client/app/hostBill/hostBill.controller.js
@@ -20,13 +20,20 @@
     // load data when *not* on page refresh
     self.data = appFactory.data;
 
+    if (!appFactory.data) {
+      socketFactory.init();
+      appFactory.initListeners();
+    }
+
     self.getDishIndivCost = appFactory.getDishIndivCost;
     self.getUsersByDish = appFactory.getUsersByDish;
 
-    self.test=function(){console.log("test"); };
     self.taxType = 'percent';
 
     self.getTax = function() {
+      if (!self.data) {
+        return 0;
+      }
       if (self.taxType === 'dollar') {
         return self.tax;
       } else if (self.taxType === 'percent') {
@@ -35,10 +42,11 @@
     };
 
     self.getTaxPercent = function() {
+      if (!self.data) {
+        return 0;
+      }
       if (self.taxType === 'dollar') {
-        console.log("getSubTotal: ", self.getSubTotal(self.data.dishes)); 
-        console.log("self.tax: ", self.tax); 
-        var num = self.tax/self.getSubTotal(self.data.dishes) * 100;
+        var num = self.tax / self.getSubTotal(self.data.dishes) * 100;
         return Math.round(num * 100) / 100; // round to 2 decimal places
       } else if (self.taxType === 'percent') {
         return self.tax;
