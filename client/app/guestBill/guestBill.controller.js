aef2b873a1720b89e51c6e7560202a590af0e4ee
Faline10 <fawn.be@gmail.com>
2015-12-19 12:14:02 -0800

    working on grandtotal bug

--- client/app/guestBill/guestBill.controller.js
+++ client/app/guestBill/guestBill.controller.js
@@ -44,15 +44,6 @@
       });
     };
 
-    self.getGuestTotal = function(data) {
-      return addDishFactory.calculateRunningTotal(data);
-    };
-
-    self.getGrandTotal = function(dishes, billData) {
-      return (!self.data) ?  0 : return _.sum(_.pluck(dishes, 'cost')) + self.getGuestTip() + self.getGuestTax();
-      }
-    };
-
     self.getOtherUsersByUsername = function(dish, users, user_id) {
       return appFactory.arrayToSentence(
         _(dish.users).filter(function(id) {
@@ -67,24 +58,66 @@
     };
 
     self.getGuestTax = function() {
-      return (!self.data) ? 0 : self.data.billData.taxPercent * self.getGuestTotal(self.data) * 0.01;
+      return (!self.data) ? 0 : self.data.billData.taxPercent * self.getGuestSubtotal(self.data) * 0.01;
     };
 
     self.getGuestTip = function() {
-      return (!self.data) ? 0 : self.data.billData.tipPercent * self.getGuestTotal(self.data) * 0.01;
+      return (!self.data) ? 0 : self.data.billData.tipPercent * self.getGuestSubtotal(self.data) * 0.01;
     };
 
     self.getGuestFee = function () {
-      return self.data.billData.feePercent * self.getGuestTotal(self.data) * 0.01;
+      return self.data.billData.feePercent * self.getGuestSubtotal(self.data) * 0.01;
     };
 
     self.getGuestDiscount = function () {
-      return self.data.billData.discountPercent * self.getGuestTotal(self.data) * 0.01;
+      return self.data.billData.discountPercent * self.getGuestSubtotal(self.data) * 0.01;
+    };
+
+    //remove?
+
+    // self.getGuestTotal = function(data) {
+    //   return addDishFactory.calculateRunningTotal(data);
+    // };
+
+    self.getGuestSubtotal = function(data) {
+      return addDishFactory.calculateRunningTotal(data);
     };
 
     self.getGuestGrandTotal = function() {
-      return (!self.data) ? 0 : self.getGuestTotal(self.data) + self.getGuestTax() + self.getGuestTip()
+      return (!self.data) ? 0 : self.getGuestSubtotal(self.data) + self.getGuestTax() + self.getGuestTip()
       + self.getGuestFee() - self.getGuestDiscount();
+    };
+
+    self.getAllSubtotal = function (dishes) {
+      return (!self.data) ? 0 : _.sum(_.pluck(dishes, 'cost'));
+    };
+
+
+    //remove?
+    // self.getGrandTotal = function(dishes, billData) {
+    //   return (!self.data) ?  0 : return _.sum(_.pluck(dishes, 'cost')) + self.getGuestTip() + self.getGuestTax();
+    //   }
+    // };
+
+    self.getGrandTotal = function(dishes, billData) {
+
+      // return (!self.data) ? 0 : _.sum(_.pluck(dishes, 'cost')) + billData.tipPercent + billData.taxPercent;
+      // return (!self.data) ? 0 : _.sum(_.pluck(dishes, 'cost')) + self.getGuestTip() + self.getGuestTax()
+      // + self.getGuestFee() + self.getGuestDiscount(); //get host tip
+      return (!self.data) ? 0 : self.data.billData.grandTotal;
+      
+      // return (!self.data) ? 0 : (function () {
+      //   var subtotal = _.sum(_.pluck(dishes, 'cost'));
+      //   console.log("subtotal: ", subtotal);  
+      //   console.log("billData: ", billData); 
+      //   console.log("billData.taxPercent: ", billData.taxPercent); 
+      //   console.log("billData.tipPercent * subtotal * 0.01: ", billData.tipPercent * subtotal *0.01); 
+      //   return subtotal 
+      //   + (billData.tipPercent * subtotal * 0.01) 
+      //   + (billData.taxPercent * subtotal * 0.01)
+      //   + self.getGuestFee() 
+      //   - self.getGuestDiscount()
+      // })();
     };
 
     self.logout = appFactory.logout;
