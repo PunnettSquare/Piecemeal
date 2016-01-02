5a3978f708c961d2e889d2a52038e777a05cf992
Faline10 <fawn.be@gmail.com>
2016-01-02 13:09:51 -0800

    Cleanup tests

--- test/spec.js
+++ test/spec.js
@@ -1,6 +1,5 @@
 // # JASMINE TESTS
-// Notes: May need to comment out calculateRunningTotal in addDish.factory to get this to work
-// run with karma start karma.conf.js
+// Run with karma start karma.conf.js
 
 beforeEach(module('Piecemeal'));
 
@@ -55,7 +54,7 @@ describe('Piecemeal', function() {
       return dish.cost / dish.users.length;
     },
     data: {
-      billData: { // billData
+      billData: {
         billSent: true,
         discountPercent: 3.33,
         feePercent: 6.67,
@@ -74,8 +73,8 @@ describe('Piecemeal', function() {
         // discountPercent: 3.33,
         // grandTotal: 18.5
       },
-      code: "mints", // code
-      dishes: [{ // dishes
+      code: "mints",
+      dishes: [{
           cost: "10.00",
           dish_id: 25,
           email: null,
@@ -92,8 +91,8 @@ describe('Piecemeal', function() {
           phone: null,
           users: [45, 46]
       }], 
-      event_id: 36, // event id
-      users: [{ // users (2 objs). first obj:
+      event_id: 36,
+      users: [{ // first user
         host: true,
         id: 45,
         status: true,
@@ -120,7 +119,7 @@ describe('Piecemeal', function() {
               phone: null,
               users: [45, 46]
         }]
-      }, { // second obj:
+      }, { // second user
         host: false,
         id: 46,
         status: false,
@@ -156,6 +155,70 @@ describe('Piecemeal', function() {
   var scopeMock = {
     $on: function() {}
   };
+
+  /* 
+  Mock for socket.io. 
+  Use later with AllDishesCtrl tests
+  see: https://github.com/btford/angular-socket-io-seed/issues/4
+  https://github.com/hackify/hackify-server/blob/master/test/controllers.test.js
+  https://github.com/nullivex/angular-socket.io-mock
+  */
+
+  var sockMock = function($rootScope) {
+    this.events = {};
+    this.emits = {};
+
+    this.init = function() {
+      console.log("sockMock init");
+    };
+
+    // Intercept 'on' calls and capture the callbacks
+    this.on = function(eventName, callback) {
+      console.log("socketMock hears eventName: ", eventName);
+      console.log("socketMock listener has this callback: ", callback); // To see whole callback, use callback.toString()
+
+      if (!this.events[eventName]) {
+        this.events[eventName] = [];
+      }
+      this.events[eventName].push(callback);
+      callback(this.emits[eventName]);
+
+      console.log("socketMock has updated this.events to: ", this.events);
+      console.log('passing socketMock.emits[eventName] to callback in this.on: ', this.emits[eventName]);
+    };
+
+    // Intercept 'emit' calls from the client and record them to assert against in the test
+    this.emit = function(eventName, data) {
+      this.emits[eventName] = data;
+      console.log('socketMock.emits["shareDish"] is: ', socketMock.emits["shareDish"]);
+    };
+    // Simulate an inbound message to the socket from the server (only called from the test)
+    this.receive = function(eventName) {
+      var args = Array.prototype.slice.call(arguments, 1);
+
+      if (this.events[eventName]) {
+        angular.forEach(this.events[eventName], function(callback) {
+          $rootScope.$apply(function() {
+            callback.apply(this, args);
+          });
+        });
+      }
+    };
+
+  };
+
+// SocketMock test format:
+
+// it("emits and receives messages", function(){
+//   var testReceived = false;
+
+//   socket.on("test", function(data){
+//     testReceived = true;
+//   });
+
+//   socket.emit("test", { info: "test" });
+//   expect(testReceived).toBe(true);
+// });
 
   // ## Unit Tests
 
@@ -200,7 +263,7 @@ describe('Piecemeal', function() {
 
   describe('HostBillCtrl', function() {
     var controller;
-    // Can use dishMock instead of appFactoryMock.data.dishes[0] for simplicity
+    // Could use dishMock instead of appFactoryMock.data.dishes[0] for simplicity
     // var dishMock = {
     //     cost: "5.00",
     //     dish_id: 26,
@@ -254,59 +317,19 @@ describe('Piecemeal', function() {
     });
 
     it('should get discount', function () {
-      expect(controller.discount).toEqual(.5);
+      expect(controller.discount).toEqual(0.5);
     });
 
-    // it('should calculate grand total', function () {
-    //   console.log('controller.getSubTotal(controller.data.dishes): ', controller.getSubTotal(controller.data.dishes)); 
-    //   console.log("controller.getTip('dollar'): ", controller.getTip('dollar')); 
-    //   console.log("controller.getTax('dollar'): ", controller.getTax('dollar')); 
-    //   console.log('controller.fee: ', controller.fee); 
-    //   console.log('controller.discount: ', controller.discount); 
-    //   expect(controller.getGrandTotal()).toEqual(18.5);
+    it('should calculate grand total', function () {
+      expect(controller.getGrandTotal()).toEqual(18.5);
       
-    // });
+    });
 
   });
 
-  // NO LONGER EXISTS:
-  // describe('AddDishCtrl', function() {
-  //   var controller, allDishesFactory;
-  //   spyOn($.fn, 'leanModal').and.returnValue("something");
+// ## Insert new tests here
 
-  //   beforeEach(function() {
-  //     allDishesFactory = {
-  //       calculateRunningTotal: function() {}
-  //     };
-  //     appFactory = {
-  //       getSessStorage: function() {},
-  //       initListeners: function() {},
-  //       shareDish: function() {}
-  //     };
-
-  //     controller = $controller('AddDishCtrl', {
-  //       $scope: scopeMock,
-  //       socketFactory: socketMock,
-  //       allDishesFactory: allDishesFactory,
-  //       appFactory: appFactory
-  //     });
-  //   });
-
-  //   it('should emit "addDish" and a dish object', function() {
-  //     controller.addDish("ramen", 10);
-  //     var testReceived = false;
-
-  //     // Hacky way to check and update testReceived. socketMock.on can be used for the same thing once functional
-  //     if (socketMock.emits["addDish"].name === "ramen" && socketMock.emits["addDish"].cost === 10) {
-  //       testReceived = true;
-  //     }
-
-  //     expect(testReceived).toBe(true);
-  //   });
-
-  // });
-
-// REQUIRES STUB OF JQUERY WHICH DOESNT WORK:
+// To test AllDishes, need to comment out jQuery leanModal method, or use a spy, which doesn't currently work:
 //   describe('AllDishesCtrl', function() {
 
 //     var controller;
@@ -334,168 +357,43 @@ describe('Piecemeal', function() {
 //     });
 
 //   });
-});
-/*
-Mock for socket.io
-see: https://github.com/btford/angular-socket-io-seed/issues/4
-https://github.com/hackify/hackify-server/blob/master/test/controllers.test.js
-https://github.com/nullivex/angular-socket.io-mock
-*/
-// When finished with tests, move this out of global scope, and into the Piecemeal describe block at top
-var sockMock = function($rootScope) {
-  this.events = {};
-  this.emits = {};
 
-  this.init = function() {
-    console.log("sockMock init");
-  };
+// Move old AddDishCtrl tests (which include sockMock tests) into  AllDishCtrl tests when jQuery spy is done:
 
-  // intercept 'on' calls and capture the callbacks
-  this.on = function(eventName, callback) {
-    console.log("socketMock hears eventName: ", eventName);
-    console.log("socketMock listener has this callback: ", callback); // to see whole callback: callback.toString()
+// describe('AddDishCtrl', function() {
+//   var controller, allDishesFactory;
+//   spyOn($.fn, 'leanModal').and.returnValue("something");
 
-    if (!this.events[eventName]) {
-      this.events[eventName] = [];
-    }
-    this.events[eventName].push(callback);
-    callback(this.emits[eventName]);
+//   beforeEach(function() {
+//     allDishesFactory = {
+//       calculateRunningTotal: function() {}
+//     };
+//     appFactory = {
+//       getSessStorage: function() {},
+//       initListeners: function() {},
+//       shareDish: function() {}
+//     };
 
-    console.log("socketMock has updated this.events to: ", this.events);
-    console.log('passing socketMock.emits[eventName] to callback in this.on: ', this.emits[eventName]);
-  };
-
-  // intercept 'emit' calls from the client and record them to assert against in the test
-
-  this.emit = function(eventName, data) {
-    // console.log("socketMock is emitting! eventName: ", eventName);
-    // console.log("socketMock is emitting this data: ", data);
-
-    // cut original code:
-    // if(this.events[eventName]){
-    //   angular.forEach(this.events[eventName], function(callback){
-    //     $rootScope.$apply(function() {
-    //       callback(data);
-    //     });
-    //   });
-    // };
-    console.log(".emit data: ", data);
-    this.emits[eventName] = data;
-    console.log('socketMock.emits["shareDish"] is: ', socketMock.emits["shareDish"]);
-  };
-  //simulate an inbound message to the socket from the server (only called from the test)
-  this.receive = function(eventName) {
-    var args = Array.prototype.slice.call(arguments, 1);
-
-    if (this.events[eventName]) {
-      angular.forEach(this.events[eventName], function(callback) {
-        $rootScope.$apply(function() {
-          callback.apply(this, args);
-        });
-      });
-    }
-  };
-
-};
-
-// socketMock test format:
-
-// it("emits and receives messages", function(){
-//   var testReceived = false;
-
-//   socket.on("test", function(data){
-//     testReceived = true;
+//     controller = $controller('AddDishCtrl', {
+//       $scope: scopeMock,
+//       socketFactory: socketMock,
+//       allDishesFactory: allDishesFactory,
+//       appFactory: appFactory
+//     });
 //   });
 
-//   socket.emit("test", { info: "test" });
-//   expect(testReceived).toBe(true);
+//   it('should emit "addDish" and a dish object', function() {
+//     controller.addDish("ramen", 10);
+//     var testReceived = false;
+
+//     // Hacky way to check and update testReceived. socketMock.on can be used for the same thing once functional
+//     if (socketMock.emits["addDish"].name === "ramen" && socketMock.emits["addDish"].cost === 10) {
+//       testReceived = true;
+//     }
+
+//     expect(testReceived).toBe(true);
+//   });
+
 // });
 
-/*
-// ANGULAR EXAMPLE
-describe('PasswordController', function() {
-  beforeEach(module('app'));
-
-  var $controller;
-
-  beforeEach(inject(function(_$controller_){
-    // The injector unwraps the underscores (_) from around the parameter names when matching
-    $controller = _$controller_;
-  }));
-
-  describe('$scope.grade', function() {
-    var $scope, controller;
-
-    beforeEach(function() {
-      $scope = {};
-      controller = $controller('PasswordController', { $scope: scopeMock });
-    });
-
-    it('sets the strength to "strong" if the password length is >8 chars', function() {
-      $scope.password = 'longerthaneightchars';
-      $scope.grade();
-      expect($scope.strength).toEqual('strong');
-    });
-
-    it('sets the strength to "weak" if the password length <3 chars', function() {
-      $scope.password = 'a';
-      $scope.grade();
-      expect($scope.strength).toEqual('weak');
-    });
-  });
 });
-
-// ANGULAR EXAMPLE - w/ SERVICE
-
-// Defined out reference variable outside
-var myService;
-
-// Wrap the parameter in underscores
-beforeEach( inject( function(_myService_){
-  myService = _myService_;
-}));
-
-// Use myService in a series of tests.
-it('makes use of myService', function() {
-  myService.doStuff();
-});
-
-// Sources: https://docs.angularjs.org/api/ngMock/function/angular.mock.inject
-https://docs.angularjs.org/guide/unit-testing
-
-// SIMPLE EXAMPLE
-describe(‘Hello World example ’, function() {
-
-beforeEach(module(‘myApp’));
-
-var HelloWorldController,
-scope;
-
-beforeEach(inject(function ($rootScope, $controller) {
-scope = $rootScope.$new();
-HelloWorldController = $controller('HelloWorldController', {
-$scope: scope
-});
-}));
-it('says hello world!', function () {
-expect(scope.greeting).toEqual("Hello world!");
-});
-
-});
-
-//SIMPLE EXAMPLE PART II
-describe('Calculator ', function() {
-
-// necessary
-it('should add two numbers correctly', function() {});
-it('should subtract two numbers correctly', function() {});
-
-// helpful but not needed
-it('should add negative numbers', function() { });
-it('should reject non numbers', function() { });
-});
-
-https://github.com/karma-runner/karma-jasmine
-later: https://github.com/karma-runner/grunt-karma and http://www.sitepoint.com/testing-javascript-jasmine-travis-karma/
-if server side testing with node: https://www.npmjs.com/package/jasmine-node-karma OR browserify OR requireJS
-*/
