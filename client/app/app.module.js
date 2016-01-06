2088a63cc6a4fe135c92298cc9c996ad3e0e673a
Michelle Lee <michelle.mh.lee92@gmail.com>
2016-01-05 18:05:30 -0800

    (doc) Docco for client-side; got rid of some CDN's

--- client/app/app.module.js
+++ client/app/app.module.js
@@ -1,10 +1,10 @@
-// # Piecemeal App Module 
+// # Piecemeal App Module
 
 // ##### [Back to Table of Contents](./tableofcontents.html)
 
 // **Summary**: Initialize Piecemeal application.
 
-angular.module('Piecemeal', ['ui.router', 'ngMessages'])
+angular.module('Piecemeal', ['ui.router'])
 
 // Set lodash ```_``` as a property of window
   .constant('_', window._)
